7700adb58d7830ec9d7fc400c7bfbb39baf2afd0
gleyver <gleyver@stillos.com.br>
2019-08-15 13:00:11 -0300

    Like and Dislike

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikesController = require('./controllers/DislikesController');
 
 const routes = express.Router();
 
 // GET, POST, PUT, DELETE
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikesController.store);
 
 module.exports = routes;
